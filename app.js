3e81386f9daae648b44b995bb40887bcca0d941f
OlenaHras <alena.hras@gmail.com>
2023-06-01 21:48:16 +0300

    add cors

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3001;
+app.use(cors());
 
 app.get("/shops", (req, res) => res.type("json").send(json));
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 const json = `
