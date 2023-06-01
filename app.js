5693e48cc5e6ce1f29d1d29b4855444c4ed4eef3
OlenaHras <alena.hras@gmail.com>
2023-06-01 21:39:38 +0300

    add route

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type("json").send(json));
+app.get("/shops", (req, res) => res.type("json").send(json));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
